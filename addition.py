9ab070c123dd7c38195954c92768b4b8c1893ac3
chandankumar5 <109968715+chandankumar5@users.noreply.github.com>
2022-12-07 19:36:55 +0530

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,6 @@
+num1=int(input("enter your first number:"))
+num2=int(input("enter your second number:"))
+num3=int(input("enter your third number:"))
+sum=num1+num2+num3
+print("the sum of three number are:num1,num2,num3")
+print(sum)
